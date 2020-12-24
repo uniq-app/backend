6d2bb1a1da0a075a35973e3d35d162bf2666a5a4
Bartłomiej Olszanowski <baloo@int.pl>
2020-12-24 02:37:33 +0100

    B-6 Update board with description and cover + swagger-ui

--- src/main/java/pl/uniq/board/service/BoardService.java
+++ src/main/java/pl/uniq/board/service/BoardService.java
@@ -2,23 +2,27 @@ package pl.uniq.board.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.uniq.exceptions.ResourceNotFoundException;
 import pl.uniq.board.models.Board;
 import pl.uniq.board.repository.BoardRepository;
+import pl.uniq.photo.models.Photo;
+import pl.uniq.photo.repository.PhotoRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class BoardService {
 
 	private final BoardRepository boardRepository;
+	private final PhotoRepository photoRepository;
 
 	@Autowired
-	public BoardService(BoardRepository boardRepository) {
+	public BoardService(BoardRepository boardRepository, PhotoRepository photoRepository) {
 		this.boardRepository = boardRepository;
+		this.photoRepository = photoRepository;
 	}
 
 	public Page<Board> findAll(Pageable page) {
@@ -38,6 +42,8 @@ public class BoardService {
 		Board storedBoard = findById(uuid);
 		if (board.getName() != null)
 			storedBoard.setName(board.getName());
+		if (board.getDescription() != null)
+			storedBoard.setDescription(board.getDescription());
 		if (board.getCreatorId() != null)
 			storedBoard.setCreatorId(board.getCreatorId());
 		if (board.getIsPrivate() != null)
@@ -49,5 +55,21 @@ public class BoardService {
 
 	public void delete(Board board) {
 		boardRepository.delete(board);
+	}
+
+	public Photo getCover(UUID uuid) {
+		Board storedBoard = findById(uuid);
+		if (storedBoard.getCover() != null)
+			return storedBoard.getCover();
+		throw new ResourceNotFoundException("This board id: " + uuid + " has no cover!");
+	}
+
+	public Board updateCover(UUID uuid, Photo photo) {
+		Board storedBoard = findById(uuid);
+		Optional<Photo> storedPhoto = photoRepository.findById(photo.getPhoto_id());
+		storedPhoto.ifPresentOrElse(storedBoard::setCover, ()->{
+			storedBoard.setCover(photo);
+		});
+		return boardRepository.save(storedBoard);
 	}
 }
