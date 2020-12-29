21bb5e30c4e5c5960a1d1f4de918fcf7b1843b49
darullef <darullef4@gmail.com>
2020-12-29 16:45:21 +0100

    Database fix

--- src/main/java/pl/uniq/auth/security/jwt/JwtAuthFilter.java
+++ src/main/java/pl/uniq/auth/security/jwt/JwtAuthFilter.java
@@ -3,7 +3,6 @@ package pl.uniq.auth.security.jwt;
 import io.jsonwebtoken.MalformedJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,8 +12,10 @@ import pl.uniq.auth.security.userdetails.CustomUserDetailsService;
 import pl.uniq.exceptions.AuthorizationHeaderException;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
 
@@ -26,7 +27,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 	}
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 		try {
 			JwtUtil jwtUtil = new JwtUtil();
 			String authorizationHeader = request.getHeader("Authorization");
@@ -38,11 +39,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 			UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
 			Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 			SecurityContextHolder.getContext().setAuthentication(authentication);
-			filterChain.doFilter(request, response);
-		} catch (MalformedJwtException | InsufficientAuthenticationException e) {
+		} catch (MalformedJwtException e) {
 			logger.warn("JWT Token is incorrect. Authentication process failed");
 		} catch (Exception e) {
 			logger.warn(e.getMessage());
 		}
+		filterChain.doFilter(request, response);
 	}
 }
