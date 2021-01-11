338b90a5999968920fe3d08e82ef9e47beb262f8
Bartłomiej Olszanowski <baloo@int.pl>
2021-01-12 00:00:32 +0100

    B-28 New notification on adding photos

--- src/main/java/pl/uniq/photo/service/PhotoService.java
+++ src/main/java/pl/uniq/photo/service/PhotoService.java
@@ -2,6 +2,7 @@ package pl.uniq.photo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.uniq.board.service.BoardService;
 import pl.uniq.exceptions.ResourceNotFoundException;
 import pl.uniq.photo.models.Photo;
 import pl.uniq.photo.repository.PhotoRepository;
@@ -14,21 +15,25 @@ import java.util.UUID;
 public class PhotoService {
 
 	private final PhotoRepository photoRepository;
+	private final BoardService boardService;
 
 	@Autowired
-	public PhotoService(PhotoRepository photoRepository) { this.photoRepository = photoRepository; }
+	public PhotoService(PhotoRepository photoRepository, BoardService boardService) {
+		this.photoRepository = photoRepository;
+		this.boardService = boardService;
+	}
 
 
 	public List<Photo> findAllByBoard(UUID uuid) throws ResourceNotFoundException {
 		return photoRepository.findAllByBoard(uuid);
 	}
 
-
 	public List<Photo> save(List<Photo> photos, UUID board_id) {
 		for (Photo photo : photos) {
 			photo.setBoard(board_id);
 			photoRepository.save(photo);
 		}
+		boardService.notifyFollowers(board_id, photos.size());
 		return photos;
 	}
 
